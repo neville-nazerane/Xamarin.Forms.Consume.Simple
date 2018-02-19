d208a5b35435bc1bd437d1048050517afa103469
Neville <live.neville@live.com>
2018-02-19 04:30:27 -0500

    Added modelhandler for xamarin

---
+++ ModelHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Xamarin.Forms.Consume.Simple
+{
+    public class ModelHandler<TModel> : ConsumeAPI.UI.Simple.ModelHandler<TModel>
+        where TModel : new()
+    {
+
+        public void Link<T>(Expression<Func<TModel, T>> lamda, Entry entry, StackLayout layout)
+            => Link<T, Entry, StackLayout, ModelEntryBinder<T>, StackErrorBinder>(lamda, entry, layout);
+
+    }
+}
